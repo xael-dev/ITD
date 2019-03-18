4b335d7e545959f41231f2edc5db856a86becc7b
Scip-io <olieyx.ah@gmail.com>
2019-03-18 16:32:45 -0400

    Reconfigure database credentials for django

--- WebsiteITD/settings.py
+++ WebsiteITD/settings.py
@@ -77,8 +77,8 @@ WSGI_APPLICATION = 'WebsiteITD.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # 'django.db.backends.sqlite3',
-        'NAME': 'itd_prod', # os.path.join(BASE_DIR, 'db.sqlite3'),
-        'USER': 'u_itadmin',
+        'NAME': 'ITDdb', # os.path.join(BASE_DIR, 'db.sqlite3'),
+        'USER': 'applicationusr',
         'PASSWORD': '1122332211',
         'HOST': 'localhost',
         'PORT': '',
