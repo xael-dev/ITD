b717670ea58c2ff1484c4fe08f31ae97d086c413
Scip-io <olieyx.ah@gmail.com>
2019-03-18 16:32:14 -0400

    Add exclusivity for server security access

--- WebsiteITD/settings.py
+++ WebsiteITD/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ''
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['157.230.62.251']
 
 
 # Application definition
