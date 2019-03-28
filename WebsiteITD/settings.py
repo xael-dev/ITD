206ea305876afe5e510bbdafb494722b92805295
Scip-io <olieyx.ah@gmail.com>
2019-03-28 12:58:33 -0400

    Undo Test Fix

--- WebsiteITD/settings.py
+++ WebsiteITD/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'DJ_SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['itdoctors.com', 'www.itdoctors.com', '142.93.56.120', 'localhost']
 
