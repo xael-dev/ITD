ea9f6cd09b7213431fb9693b1540cfad974bb65b
Scip-io <olieyx.ah@gmail.com>
2019-04-17 16:54:43 -0400

    Update settings.py for production

--- WebsiteITD/settings.py
+++ WebsiteITD/settings.py
@@ -74,30 +74,30 @@ WSGI_APPLICATION = 'WebsiteITD.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         # 'django.db.backends.sqlite3',
-#         'NAME': 'itd',
-#         # os.path.join(BASE_DIR, 'db.sqlite3'),
-#         'USER': 'itduser',
-#         'PASSWORD': '1122332211',
-#         'HOST': 'localhost',
-#         'PORT': '',
-#     }
-# }
-
-#Database settings for local tests
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # 'django.db.backends.sqlite3',
+        'NAME': 'itd',
+        # os.path.join(BASE_DIR, 'db.sqlite3'),
+        'USER': 'itduser',
+        'PASSWORD': '1122332211',
+        'HOST': 'localhost',
         'PORT': '',
     }
 }
+
+#Database settings for local tests
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         'USER': '',
+#         'PASSWORD': '',
+#         'HOST': '',
+#         'PORT': '',
+#     }
+# }
 
 
 # Password validation
