6c17d47aea5625b5230b5ac4455f81f945064d59
Scip-io <olieyx.ah@gmail.com>
2019-04-17 13:49:12 -0400

    Add crispyforms app

--- WebsiteITD/settings.py
+++ WebsiteITD/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -51,6 +52,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'WebsiteITD.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
