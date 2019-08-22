b3cc2ac35c057dca5e30dfbb4db66001c6d34e57
IT <ajharris8814@gmail.com>
2019-08-22 16:32:09 +0000

    Update binary files

---
+++ static/js/javascript.js
@@ -0,0 +1,8 @@
+function expandFunction() {
+    var x = document.getElementById("navbar")
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar";
+    }
+}
